e06fb707c113b1cb012a63088114eb18b6fd4fc9
Vladislav Savionok <savionak@gmail.com>
2017-03-21 12:45:53 +0300

    Add maps

---
+++ Informer/ViewController/FullMapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FullMapViewController.swift
+//  Informer
+//
+//  Created by Admin on 3/21/17.
+//  Copyright © 2017 BSUIR. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FullMapViewController: UIViewController {
+    
+    @IBOutlet weak var FullMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
